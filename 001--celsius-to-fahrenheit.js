63fdf3da68f1c16d25cba594485b3eb06a5b9303
Viren Bhagat <virenb@gmail.com>
2020-03-12 20:13:59 -0400

    Added first challenge (yet to be completed)

---
+++ 001--celsius-to-fahrenheit.js
@@ -0,0 +1,25 @@
+/* Basic Algorithm Scripting: Convert Celsius to Fahrenheit 
+
+The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, 
+plus 32. You are given a variable celsius representing a temperature in Celsius. Use the 
+variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to 
+the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius
+ temperature to Fahrenheit.
+ 
+ */
+
+function convertToF(celsius) {
+  let fahrenheit;
+  return fahrenheit;
+}
+
+convertToF(30);
+
+
+// TESTS
+// convertToF(0) should return a number
+// convertToF(-30) should return a value of - 22
+// convertToF(-10) should return a value of 14
+// convertToF(0) should return a value of 32
+// convertToF(20) should return a value of 68
+// convertToF(30) should return a value of 86
