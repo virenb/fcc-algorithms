45e540d18ff2ce06d26aab7f7905dbfc43adfe14
Viren Bhagat <virenb@gmail.com>
2020-03-29 17:34:44 -0400

    Added problem # 2

---
+++ 002---reverse-a-string.js
@@ -0,0 +1,19 @@
+/* Basic Algorithm Scripting: Reverse a String
+
+Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.
+Your result must be a string.
+
+ */
+
+function reverseString(str) {
+  return str;
+}
+
+reverseString("hello");
+
+// TESTS
+// reverseString("hello") should return a string.
+// reverseString("hello") should become "olleh".
+// reverseString("Howdy") should become "ydwoH".
+// reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
