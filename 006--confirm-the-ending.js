e9da647380c75fcf5ffd5d948ca25da59ea87ddf
Viren Bhagat <virenb@gmail.com>
2020-04-09 20:30:22 -0400

    Solved 006

--- 006--confirm-the-ending.js
+++ 006--confirm-the-ending.js
@@ -8,11 +8,22 @@ This challenge can be solved with the .endsWith() method, which was introduced i
 
 // Thoughts
 /*
-
+Read the description carefully
+DO NOT use .endsWith() method (introducted in ES2015)
+TRY to use a substring() method
+It is okay to check the documentation of this method
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+Problem gives us two inputs, the string, and the target string
+The string prototype of length will be used
+We also want the length of the target string
+Reading the docs of substring(), it can take in more than one argument
+str.substring(x) x will be the index to start the string from
+"Bastian".substring(2) == "stian"
+So if we subtract target length from string length, we will get the string from the index we want to compare
 */
 
 function confirmEnding(str, target) {
-  return str;
+  return str.substring(str.length - target.length) == target;
 }
 
 confirmEnding("Bastian", "n");
