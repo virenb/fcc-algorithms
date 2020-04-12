b9540ffc12c6105455f5fc4e39b00974000ec185
Viren Bhagat <virenb@gmail.com>
2020-04-12 16:10:34 -0400

    Solved boo who problem

--- 010---boo-who.js
+++ 010---boo-who.js
@@ -8,11 +8,17 @@ Boolean primitives are true and false.
 
 // Thoughts
 /*
-
+Read the problem a few times. It was a little confusing
+I thought we'd have to check for truthy or falsy values but have a look at the tests
+The problem is only asking to check if the argument is a boolean primitive
+If it is true or false, not truthy or falsy values
+So if argument is false, we return true. If argument is true, we return true. That is it
+So we can just check typeof argument, if its a boolean, we return true
+Otherwise every other type is false
 */
 
 function booWho(bool) {
-  return bool;
+  return typeof bool == 'boolean';
 }
 
 booWho(null);
