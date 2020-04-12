e0c681a32f0071b132f34399af271a86accc3f29
Viren Bhagat <virenb@gmail.com>
2020-04-12 16:22:54 -0400

    Set up title case a sentence problem

---
+++ 011---title-case-a-sentence.js
@@ -0,0 +1,28 @@
+/* Basic Algorithm Scripting: Title Case a Sentence
+
+Return the provided string with the first letter of each word capitalized. 
+Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting 
+words like "the" and "of".
+
+ */
+
+
+// Thoughts
+/*
+
+*/
+
+function titleCase(str) {
+  return str;
+}
+
+titleCase("I'm a little tea pot");
+
+
+// TESTS
+// titleCase("I'm a little tea pot") should return a string.
+// titleCase("I'm a little tea pot") should return I'm A Little Tea Pot.
+// titleCase("sHoRt AnD sToUt") should return Short And Stout.
+// titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return Here Is My Handle Here Is My Spout
