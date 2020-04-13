6035ff4d993385a237c3cbd27033773414a1836b
Viren Bhagat <virenb@gmail.com>
2020-04-13 17:11:07 -0400

    Solution for slice and splice

--- 012---slice-and-splice.js
+++ 012---slice-and-splice.js
@@ -9,10 +9,24 @@ Return the resulting array. The input arrays should remain the same after the fu
 
 // THOUGHTS
 /*
+We're given two arrays and an index
+Ultimately, we want to insert the first array's values into the second array at the correct index
+Looking at the tests, we do not want to alter arr1 or arr2 so we should make a copy
+Since we're inserting into second array, we can make a new variable and set it to ...arr2
+we can now splice the new variable
+array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+So the input n is the index we want to start the insert from so that will be our first argument
+We do not want to delete any, we want to add from arr1 so the second argument should be 0
+the third argument should be arr1 
+splice(n, 0, arr1)
+If we do this, we get an array inside an array, we do not want that.
+We can use the spread operator instead of using arr1 
 */
 
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  let newArr2 = [...arr2];
+  newArr2.splice(n, 0, ...arr1);
+  return newArr2;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
