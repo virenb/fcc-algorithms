031e9ce2ca3618330bc0ce4c7a99055267eb9d24
Viren Bhagat <virenb@gmail.com>
2020-04-14 18:40:24 -0400

    Solution for Falsy Bouncer

--- 013---falsy-bouncer.js
+++ 013---falsy-bouncer.js
@@ -8,15 +8,33 @@ Hint: Try converting each value to a Boolean.
 
 // THOUGHTS
 /*
-
+We're given an array as input
+We want to go through the array and see which values are falsy
+The instruction mentions what is falsy (false, null, 0, "", undefined, NaN)
+The hint says to convert each item into a Boolean object
+You can wrap any value and it will return true or false
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+We can look at the newer array methods instead of a for loop
+Usually turn to map() but there is a filter() method
+It will return a new array with the values which pass what we propose
+This will return only true values
 */
 
 function bouncer(arr) {
-  return arr;
+  return arr.filter(function (elem) {
+    return Boolean(elem);
+  })
 }
 
 bouncer([7, "ate", "", false, 9]);
 
+// ALTERNATIVE SOLUTION
+// Can use an arrow function to shorten the code a little, still pretty clear
+/* function bouncer(arr) {
+  return arr.filter(elem => Boolean(elem));
+}
+
+*/
 
 // TESTS
 // bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
