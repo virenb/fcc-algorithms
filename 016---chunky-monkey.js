7f710a7f88b38b5b5a9159b502d423b8d870a2f0
Viren Bhagat <virenb@gmail.com>
2020-04-17 15:14:46 -0400

    Solution to chunky monkey

--- 016---chunky-monkey.js
+++ 016---chunky-monkey.js
@@ -5,14 +5,32 @@ the length of size (second argument) and returns them as a two-dimensional array
 
 */
 
-
 // Thoughts
 /*
-
+Our input is an array and a number
+Our output must be a two-dimensional array 
+We have to make arrays within our given array, based on the size argument
+We will have to cycle through the array, I will use a for loop
+We will use splice() to remove elements from the array and put into a new array
+then have to insert the newly created arrays back into the original array (arr)
+we will set variable, i, to zero and loop until it reaches the arr.length
+we will call splice with splice(i, size) as size is the value of how big
+our new arrays should be
+we will then take those newly formed arrays and insert into the front of arr
+using unshift()
+Using console log a lot to check what is in each array
+once the for loop is finished, our arr is actually backwards so we can return
+arr.reverse() to get the desired order
+Each loop, it will take the next index, slice two elements, then add in front
+as an array of the size variable
 */
 
 function chunkArrayInGroups(arr, size) {
-  return arr;
+  for (let i = 0; i < arr.length; i++) {
+    let toSplice = arr.splice(i, size);
+    arr.unshift(toSplice);
+  }
+  return arr.reverse();
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
