ab5e4dac9c9360193ee2ccb086e91895286cbf26
virenb <virenb@gmail.com>
2020-04-23 11:13:20 -0400

    Solution for 018

--- 018---diff-two-arrays.js
+++ 018---diff-two-arrays.js
@@ -11,12 +11,34 @@ You can return the array with its elements in any order.
 
 // Thoughts
 /*
+Our input is two arrays, we have to return an array;
+We can work on a solution by looping through the arrays but since we now have some newer methods
+(reduce, map, filter, etc) let us try to work with them
+With the loop, we can compare each array against each other
+Another way to think about this is to combine the arrays then see what repeats
+There is a `concat()` method to combine arrays, so we can start with that
+We can use a `filter()` method after that but we must determine how to filter
+The includes() method returns a true or false, so that would work well with filter
+Reading back the instructions, we want to return values found in either array, but not both.
+The filter() method will go through the combined array, if can use includes and check in 
+the original arrays arr.filter(item => !arr1.includes(item) OR !arr2.includes(item))
+the `!` meaning opposite (i.e. !false returns true)
+*/
+
+// Pseudocode
+/*
+function diffArray(arr1, arr2) {
+  combine arrays
+
+  check if each value is arr1 OR arr2
+    return values
+}
 */
 
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  return newArr;
+  let newArr = arr1.concat(arr2);
+  return newArr.filter(item => !arr1.includes(item) || !arr2.includes(item));
 }
   
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
