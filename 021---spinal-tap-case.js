7412186592bedd9ab525315c0f20fc645357fd25
virenb <virenb@gmail.com>
2020-04-28 16:26:10 -0400

    Added problem #21

---
+++ 021---spinal-tap-case.js
@@ -0,0 +1,23 @@
+/* Intermediate Algorithm Scripting: Spinal Tap Case
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+*/
+
+// THOUGHTS
+/*
+
+*/
+
+function spinalCase(str) {
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
+
+// TESTS
+// spinalCase("This Is Spinal Tap") should return "this-is-spinal-tap".
+// spinalCase("thisIsSpinalTap") should return "this-is-spinal-tap".
+// spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
+// spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
+// spinalCase("AllThe-small Things") should return "all-the-small-things".
