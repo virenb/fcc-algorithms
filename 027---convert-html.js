c366ed0a7251347b74a21ac14935c3003bc16fea
virenb <virenb@gmail.com>
2020-05-06 15:11:24 -0400

    Problem # 027 set-up

---
+++ 027---convert-html.js
@@ -0,0 +1,23 @@
+/* Intermediate Algorithm Scripting: Convert HTML Entities
+
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
+in a string to their corresponding HTML entities.
+*/
+
+
+// THOUGHTS
+
+function convertHTML(str) {
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
+
+// TESTS
+// convertHTML("Dolce & Gabbana") should return "Dolce &amp; Gabbana".
+// convertHTML("Hamburgers < Pizza < Tacos") should return "Hamburgers &lt; Pizza &lt; Tacos".
+// convertHTML("Sixty > twelve") should return "Sixty &gt; twelve".
+// convertHTML('Stuff in "quotation marks"') should return "Stuff in &quot;quotation marks&quot;".
+// convertHTML("Schindler's List") should return "Schindler&apos;s List".
+// convertHTML("<>") should return "&lt;&gt;".
+// convertHTML("abc") should return "abc".
