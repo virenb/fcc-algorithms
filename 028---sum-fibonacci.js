d30253b4c1de86fe5c4aaf860db8f113d2a1bf38
virenb <virenb@gmail.com>
2020-05-08 18:24:43 -0400

    Solution for # 028

--- 028---sum-fibonacci.js
+++ 028---sum-fibonacci.js
@@ -13,9 +13,38 @@ than or equal to 10 are 1, 1, 3, and 5.
 
 
 // THOUGHTS
+/*
+
+Our input is an integer, we need to output an integer as well
+The two complexities are dealing with fibonacci numbers and also odd numbers
+Several ways to go about this, I chose to try a for loop but eventually switched to a while loop
+Since fibonacci numbers are the sum of previous numbers, I made an array of the starting ones [1,1]
+I declared a count variable in order to loop as well
+In the loop, we will add the the two consecutive numbers then push into the array
+
+Once we reach the num limit
+We can run a filter method on the array to remove all non odd numbers
+Then we can run reduce to sum it 
+
+*/
 
 function sumFibs(num) {
-  return num;
+  let fibs = [1, 1];
+  let count = 0;
+  let fibNums;
+
+  while (num > count) {
+    fibNums = fibs[count] + fibs[count + 1];
+    
+    if (fibNums <= num) {
+      fibs.push(fibNums);
+    }
+
+    count++;
+  }
+
+  return fibs.filter(n => n % 2 !== 0).reduce((a,b) => a + b);
+
 }
 
 sumFibs(4);
