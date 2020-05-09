282628f03ef5d6928ce50ef77b2bfe8c4bdce7c9
virenb <virenb@gmail.com>
2020-05-09 15:58:52 -0400

    Solution for #029

--- 029---sum-all-primes.js
+++ 029---sum-all-primes.js
@@ -11,10 +11,36 @@ or equal to num.
 
 
 // THOUGHTS
-
+/*
+Our input, `num`, is an integet
+Our output must also be an integer, a sum
+We have to loop through all the numbers up to num and sum all the prime numbers
+We have to figure out how to check for prime numbers
+Our helper function, checks if the number is prime. We loop through numbers starting at 2
+We check the divisibility and return a boolean
+We then can use the helper function in the main function. Another for loop
+If the number is prime, add to the sum
+Return sum 
+*/
 
 function sumPrimes(num) {
-  return num;
+  let sum = 0;
+  for (let j = 1; j <= num; j++) {
+    if (isPrime(j)) {
+      sum += j;
+    }
+  }
+  return sum;
+}
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= (n/2); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 sumPrimes(10);
