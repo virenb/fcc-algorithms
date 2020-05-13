3683c0aa152c0bb293e8b00407824360786cc9e8
virenb <virenb@gmail.com>
2020-05-12 20:08:48 -0400

    Solution for #031

--- 031---drop-it.js
+++ 031---drop-it.js
@@ -11,9 +11,28 @@ arr should be returned as an empty array.
 
 
 // THOUGHTS
+/*
+Two arguments, an array and a function
+We have to return an array
+This is a bit tough, had to re-read directions multiple times.
+We do not want to create a new array, work with and return the original
+We can't just loop through and push into a new array
+We want to remove elements until we hit the first true value
+We always want to begin with the first element, so `shift()` will be of use
+If we are altering `arr`, we want to save the original lenght in a variable
+So we know how many times to loop
+We check the first arr[0], if its false then we shift() and remove that
+This repeats until we hit a true value
+*/
 
 
 function dropElements(arr, func) {
+  const arrLength = arr.length;
+  for (let i = 0; i < arrLength; i++) {
+    if (!func(arr[0])) {
+      arr.shift();
+    }
+  }
   return arr;
 }
 
