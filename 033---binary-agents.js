373a17df0407963d20617b134f1382d5637090e3
virenb <virenb@gmail.com>
2020-05-14 13:15:18 -0400

    Solution for #033

--- 033---binary-agents.js
+++ 033---binary-agents.js
@@ -7,9 +7,22 @@ The binary string will be space separated.
 
 
 // THOUGHTS
+/*
+Our input is a string (sentence) of binary items
+We need to output a string, converting the binary to alphabets
+Like other problems, we can `split(' ')` the `str` into an array
+There is a string method, `fromCharCode()` we will use
+fromCharCode() will convert the digits to letters
+We will also use parseInt() on each item in the array, converting from radix 2
+`parseInt(binaryItem, 2)`
+All of this can be chained together
+Then join() back into a string, we have an sentence in English
+*/
 
 function binaryAgent(str) {
-  return str;
+  return str.split(' ')
+            .map(b => String.fromCharCode(parseInt(b, 2)))
+            .join('');
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
