c1a8df6d27d83967804762e3069c251496bb9521
virenb <virenb@gmail.com>
2020-05-15 14:07:03 -0400

    Solution for #034

--- 034---everything-be-true.js
+++ 034---everything-be-true.js
@@ -16,10 +16,25 @@ Remember, you can access object properties through either dot notation or [] not
 
 
 // THOUGHTS
-
+/*
+We have two arguments as input. An array of objects, and a string.
+The string is a key within the objects. We need to output a boolean value, true or false.
+We need to check every object's properties, if they have `pre` and then if the value is 
+truthy
+So if the value is not an empty string, 0, undefined, NaN, null
+Luckily JS comes with a built in object, `Boolean`
+We can map through the array, checking if the object property exists and if its truthy
+or not
+Since we are mapping, we'll return an array of T and F
+We can just check with `includes()` if there is a false value, then we will return false
+*/
 
 function truthCheck(collection, pre) {
-  return pre;
+  let checkedArr = collection.map(obj => {
+    return obj.hasOwnProperty(pre) && Boolean(obj[pre]);
+  })
+
+  return checkedArr.includes(false) ? false : true;
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
